84778d1012610e9665a09829718127b309456465
brownben <bonalybob@users.noreply.github.com>
2017-12-09 11:11:14 +0000

    Add Entries to Loki DB
    - Add to Entries
    - Add Credits
    - Add package to package.json
    - Add databases to gitignore

--- entries.js
+++ entries.js
@@ -3,3 +3,22 @@
 //////////////////////////////////////////////////////////////////
 
 // Add and View Entries                                         //
+
+// Database
+const loki = require('lokijs');
+
+var db = new loki('./nevis.json');
+var competitors = db.addCollection('competitors');
+
+document.getElementById('entries-submit').addEventListener('click', function () {
+    competitors.insert({
+        name: document.getElementById('entries-name').value,
+        ageClass: document.getElementById('entries-age-class').value,
+        course: document.getElementById('entries-course').value
+    });
+    document.getElementById('entries-name').value = "";
+    document.getElementById('entries-age-class').value = "";
+    course: document.getElementById('entries-course').value = "";
+    db.saveDatabase();
+    console.log(competitors.data)
+});
