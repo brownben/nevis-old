132222a381194e60871419753ece6c001a02e651
brownben <bonalybob@users.noreply.github.com>
2017-12-14 20:40:37 +0000

    Add System Information
    - Add version numbers to about

--- main-menu.js
+++ main-menu.js
@@ -5,50 +5,53 @@
 // Set up main menu and links between screens                   //
 
 // Set Up Welcome Screen
-document.getElementById('download-output').setAttribute('style', 'display:none;')
-document.getElementById('entries').setAttribute('style', 'display:none;')
-document.getElementById('download-menu').setAttribute('style', 'display:none;')
 document.getElementById('main-menu').setAttribute('style', 'display:block;')
 document.getElementById('back').setAttribute('style', 'display:none;')
+document.getElementById('download-output').setAttribute('style', 'display:none;')
+document.getElementById('download-menu').setAttribute('style', 'display:none;')
+document.getElementById('entries').setAttribute('style', 'display:none;')
 document.getElementById('entries-menu').setAttribute('style', 'display:none;')
 document.getElementById('about').setAttribute('style', 'display:none;')
+
 // If Download Clicked
 document.getElementById('menu-download').addEventListener('click', function () {
-    document.getElementById('download-output').setAttribute('style', 'display:block;')
-    document.getElementById('entries').setAttribute('style', 'display:none;')
-    document.getElementById('download-menu').setAttribute('style', 'display:block;')
     document.getElementById('main-menu').setAttribute('style', 'display:none;')
     document.getElementById('back').setAttribute('style', 'display:block;')
+    document.getElementById('download-output').setAttribute('style', 'display:block;')
+    document.getElementById('download-menu').setAttribute('style', 'display:block;')
+    document.getElementById('entries').setAttribute('style', 'display:none;')
     document.getElementById('entries-menu').setAttribute('style', 'display:none;')
     document.getElementById('about').setAttribute('style', 'display:none;')
 })
+
 // If Entries Clicked
 document.getElementById('menu-entries').addEventListener('click', function () {
-    document.getElementById('download-output').setAttribute('style', 'display:none;')
-    document.getElementById('entries').setAttribute('style', 'display:block;')
-    document.getElementById('download-menu').setAttribute('style', 'display:none;')
     document.getElementById('main-menu').setAttribute('style', 'display:none;')
     document.getElementById('back').setAttribute('style', 'display:block;')
+    document.getElementById('download-output').setAttribute('style', 'display:none;')
+    document.getElementById('download-menu').setAttribute('style', 'display:none;')
+    document.getElementById('entries').setAttribute('style', 'display:block;')
     document.getElementById('entries-menu').setAttribute('style', 'display:block;')
     document.getElementById('about').setAttribute('style', 'display:none;')
 })
 // If About Clicked
 document.getElementById('menu-about').addEventListener('click', function () {
-    document.getElementById('download-output').setAttribute('style', 'display:none;')
-    document.getElementById('entries').setAttribute('style', 'display:none;')
-    document.getElementById('download-menu').setAttribute('style', 'display:none;')
     document.getElementById('main-menu').setAttribute('style', 'display:none;')
     document.getElementById('back').setAttribute('style', 'display:block;')
+    document.getElementById('download-output').setAttribute('style', 'display:none;')
+    document.getElementById('download-menu').setAttribute('style', 'display:none;')
+    document.getElementById('entries').setAttribute('style', 'display:none;')
     document.getElementById('entries-menu').setAttribute('style', 'display:none;')
     document.getElementById('about').setAttribute('style', 'display:block;')
 })
+
 // If Back Clicked
 document.getElementById('back-button').addEventListener('click', function () {
-    document.getElementById('download-output').setAttribute('style', 'display:none;')
-    document.getElementById('entries').setAttribute('style', 'display:none;')
-    document.getElementById('download-menu').setAttribute('style', 'display:none;')
     document.getElementById('main-menu').setAttribute('style', 'display:block;')
     document.getElementById('back').setAttribute('style', 'display:none;')
+    document.getElementById('download-output').setAttribute('style', 'display:none;')
+    document.getElementById('download-menu').setAttribute('style', 'display:none;')
+    document.getElementById('entries').setAttribute('style', 'display:none;')
     document.getElementById('entries-menu').setAttribute('style', 'display:none;')
     document.getElementById('about').setAttribute('style', 'display:none;')
 });
